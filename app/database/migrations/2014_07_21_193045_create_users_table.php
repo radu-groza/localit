10eb06aae73ce2890b68c3e2e201617f4d9075bd
Radu Groza <radu.groza@live.com>
2014-07-25 21:29:57 +0300

    first commit

---
+++ app/database/migrations/2014_07_21_193045_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('email', 256)->unique();
+            $table->string('password', 64);
+            $table->string('role', 32);
+            $table->string('fb_id', 256);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+
+}
